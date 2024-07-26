cff46e2cc1445bc44f0c65a288edf746ed834139
Linette Juarez <powerangerojo3045@gmail.com>
2024-07-25 23:10:34 -0600

    Add files via upload

---
+++ Material.java
@@ -0,0 +1,17 @@
+package mx.com.cuh;
+
+public class Material implements Materiales{
+    public void mostrarNombre(){
+
+    }
+    public void mostrarExistencias(){
+
+    }
+    public boolean hacerPedido(){
+
+        return false;
+    }
+    public void mostrarPrecio(){
+
+    }
+}
